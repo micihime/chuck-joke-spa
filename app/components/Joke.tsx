9ba42bc1cb9bb4482701d87b1c677d303e173a31
Miroslava Hreskova <hroncovam@yahoo.com>
2024-12-29 15:57:53 +0100

    Moving joke component styling from page CSS module to joke component CSS module

--- app/components/Joke.tsx
+++ app/components/Joke.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "../page.module.css";
+import styles from "./Joke.module.css";
 
 interface ChuckNorrisJoke {
     categories: string[];
